d7749caa3d3e2c5a9f211599c9ba5085297f0c59
newbieski <usb1usb2@naver.com>
2022-06-24 14:51:47 +0900

    initial commit - simple hello

---
+++ src/main/java/com/example/application1/Application1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.application1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application1DemoApplication.class, args);
+	}
+
+}

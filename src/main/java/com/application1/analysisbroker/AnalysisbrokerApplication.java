61083833236a323272a1b4c708ae0c4b24328a10
newbieski <usb1usb2@naver.com>
2022-07-01 22:39:58 +0900

    initial

---
+++ src/main/java/com/application1/analysisbroker/AnalysisbrokerApplication.java
@@ -0,0 +1,13 @@
+package com.application1.analysisbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisbrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisbrokerApplication.class, args);
+	}
+
+}

e1f3e01cd50ec7992755029034dcdba9ee209dc6
newbieski <usb1usb2@naver.com>
2022-07-03 23:56:57 +0900

    lombok, dto test

---
+++ src/test/java/com/application1/analysisbroker/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.application1.analysisbroker.web.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+    @Test
+    public void lombok_test() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}

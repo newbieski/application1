9e766fef4fa39302293f8d27bdfce627a06c46a7
newbieski <usb1usb2@naver.com>
2022-07-16 00:10:36 +0900

    wordcloud - not stable

--- app.py
+++ app.py
@@ -1,24 +1,46 @@
-import mysql.connector
 from flask import Flask
 from flask import g
 from flask import render_template
 from flask import request
 from flask import Response
 
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+from collections import defaultdict
 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 
 def mycurl(url) :
     html = urlopen("https://"+url)
     bsObject = BeautifulSoup(html, "html.parser")
-    res = []
-
+    dict = defaultdict(int)
+    
     for link in bsObject.find_all('a'):
-        res.append(link.text.strip() + link.get('href'))
-    return "\n".join(res)
+        key = link.text.strip()
+        key = key.replace(" ", "")
+        dict[key] += 1    
+    
+    wordcloud = WordCloud(
+        font_path = 'NanumBarunGothic.ttf',
+        width = 800,
+        height = 800,
+        background_color="white",
+        )
+
+    wordcloud = wordcloud.generate_from_frequencies(dict)
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")    
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+    
 
 app = Flask(__name__)
 
